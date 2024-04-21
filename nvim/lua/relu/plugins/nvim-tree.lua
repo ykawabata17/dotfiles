ed5914546ab562b19794824d950c5d1dd87d26b3
ykawabata17 <yuya.kawabata@techouse.jp>
2024-04-21 19:59:28 +0900

    add: nvim-tee additional option

--- nvim/lua/relu/plugins/nvim-tree.lua
+++ nvim/lua/relu/plugins/nvim-tree.lua
@@ -2,12 +2,50 @@ return {
   "nvim-tree/nvim-tree.lua",
   dependencies = "nvim-tree/nvim-web-devicons",
   config = function()
-    local nvimtree = require("nvim-tree")
     -- 推奨されてる設定をそのまま使用
     vim.g.loaded_netrw = 1
     vim.g.loaded_netrwPlugin = 1
 
-    nvimtree.setup({
+    require("nvim-tree").setup({
+      view = {
+        width = 40,
+      },
+      renderer = {
+        highlight_modified = "all",
+        highlight_git = true,
+        indent_markers = {
+          enable = true,
+        },
+        icons = {
+          glyphs = {
+            folder = {
+              arrow_closed = "⇨",
+              arrow_open = "⇩",
+            },
+          },
+        },
+      },
+      actions = {
+        open_file = {
+          window_picker = {
+            enable = false,
+          },
+        },
+      },
+      filters = {
+        custom = { ".DS_Store" },
+      },
+      git = {
+        ignore = false,
+      },
     })
+
+    -- keybinding を設定する
+    local map = vim.keymap.set
+
+    map("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
+    map("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on cursor" })
+    map("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" })
+    map("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" })
   end
 }
