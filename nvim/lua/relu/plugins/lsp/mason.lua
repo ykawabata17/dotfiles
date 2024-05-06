c71d60406e4ba960c615ef7861b28c1a6f6ac790
ykawabata17 <yuya.kawabata@techouse.jp>
2024-05-06 16:18:29 +0900

    add: formatting plugin

--- nvim/lua/relu/plugins/lsp/mason.lua
+++ nvim/lua/relu/plugins/lsp/mason.lua
@@ -2,10 +2,13 @@ return {
   "williamboman/mason.nvim",
   dependencies = {
     "williamboman/mason-lspconfig.nvim",
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
   },
   config = function()
     -- import mson
     local mason = require("mason")
+
+    local mason_tool_installer = require("mason-tool-installer")
 
     -- import mason-lspconfig
     local mason_lspconfig = require("mason-lspconfig")
@@ -34,6 +37,15 @@ return {
         "rubocop",
         "sqlls",
       },
+    })
+
+    mason_tool_installer.setup({
+      ensure_installed = {
+        "prettier", -- prettier formatter
+        "stylua", -- lua formatter
+        "isort", -- python formatter
+        "black", -- python formatter
+      }
     })
   end,
 }
