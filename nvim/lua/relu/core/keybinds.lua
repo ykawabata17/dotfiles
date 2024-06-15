59f78164df43c551e2abbdf69f14920e9d986720
ykawabata17 <yuya.kawabata@techouse.jp>
2024-06-15 22:12:44 +0900

    add: insert mode next line

--- nvim/lua/relu/core/keybinds.lua
+++ nvim/lua/relu/core/keybinds.lua
@@ -2,7 +2,6 @@ vim.g.mapleader = " "
 
 local map = vim.keymap.set
 
-
 map("n", "<leader>ex", vim.cmd.Ex)
 
 -- 選択した行を上下に移動させる
@@ -10,9 +9,9 @@ map("v", "J", ":m '>+1<CR>gv=gv")
 map("v", "K", ":m '<-2<CR>gv=gv")
 
 -- クリップボードにコピー
-map("n", "<leader>y", "\"+y", { desc = "Copy to clipboard" })
-map("v", "<leader>y", "\"+y", { desc = "Copy to clipboard" })
-map("n", "<leader>Y", "\"+Y", { desc = "Copy to clipboard" })
+map("n", "<leader>y", '"+y', { desc = "Copy to clipboard" })
+map("v", "<leader>y", '"+y', { desc = "Copy to clipboard" })
+map("n", "<leader>Y", '"+Y', { desc = "Copy to clipboard" })
 
 -- 文字列を置換する
 map("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = "Replace string" })
@@ -24,4 +23,4 @@ map("i", "<C-h>", "<Left>", { desc = "Move Left" })
 map("i", "<C-l>", "<Right>", { desc = "Move Right" })
 map("i", "<C-j>", "<Down>", { desc = "Move Down" })
 map("i", "<C-k>", "<Up>", { desc = "Move Up" })
-
+map("i", "<C-i>", "<C-c>o", { desc = "Next line insert" })
